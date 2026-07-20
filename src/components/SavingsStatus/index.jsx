7cd1dd270172e3aea802c383cae56ae2eb18e760
Heitor Lacourt <heitorlmsantos@gmail.com>
2026-07-20 15:31:15 -0300

    Faz a estruturação inicial dos cards e finaliza o card de orçamento diário e começa meta financeira

---
+++ src/components/SavingsStatus/index.jsx
@@ -0,0 +1,11 @@
+import { IconSavings } from "../icons"
+
+export const SavingsStatus = () => {
+    return (
+        <div>
+            <p className="text-secondary-income text-xl leading-[125%] flex items-center gap-2 justify-center">
+                <IconSavings /> Economizar
+            </p>
+        </div>
+    )
+}
